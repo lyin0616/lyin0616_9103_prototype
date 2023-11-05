3506aa7b3bcccddbed007052fe3e1f37c6cba8f2
lyin0616 <145220890+lyin0616@users.noreply.github.com>
2023-11-05 19:01:58 +1100

    update

--- sketch.js
+++ sketch.js
@@ -34,6 +34,8 @@ let polyBlurry2; //the distant building
 
 let song;
 let button;
+let unitH;
+let unitH1;
 
 function preload() {
   song = loadSound("audio/drums.mp3"); // Preload the audio file
@@ -41,8 +43,11 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   button = createButton("Play"); // Create a play button
   button.mousePressed(toggleSong); // Add mouse press event to the button
+  fft = new p5.FFT(0.8, 512); // Create a new FFT analysis object
+  song.connect(fft); // Add the song into the FFT's input
 
   //Define the color arrays for lerpColor().
   //The colors are: [0]navy blue, [1]sea green, [2]bright yellow, [3]orange red, [4]dark red
@@ -54,7 +59,7 @@ function setup() {
     color(205, 74, 74)
   );
 
-  //The colors are: [0]sea green, [1]bright yellow, [2]orange red
+  //The colors are: [0]sea green, [1]bright yellow, [2]orange red, [3]dark red
   skyColorsTo.push(
     color(125, 155, 147),
     color(255, 214, 101),
@@ -62,8 +67,19 @@ function setup() {
     color(205, 74, 74)
   );
 
-  //Build four arrays: skyColorLerp A/B/C/D to contain the lerpColor() results between the
-  //skyColorsFrom[] and skyColorsTo[]
+  waterColorsFrom.push(
+    color(193, 113, 67),
+    color(255, 214, 101),
+    color(125, 155, 147),
+    color(62, 84, 143)
+  );
+
+  waterColorsTo.push(
+    color(205, 74, 74),
+    color(193, 113, 67),
+    color(255, 214, 101),
+    color(125, 155, 147)
+  );
 
   //The brushWidth of the ellipse is 1/64 of the height of canvas.
   brushWidth = height / 64;
@@ -75,25 +91,6 @@ function setup() {
 
   cols = windowWidth / scl;
   rows = windowHeight / scl;
-
-  //Define the color arrays for lerpColor().
-
-  //The colors are: [0]navy blue, [1]sea green, [2]bright yellow, [3]orange red, [4]dark red
-  waterColorsFrom.push(
-    //color(205, 74, 74),
-    color(193, 113, 67),
-    color(255, 214, 101),
-    color(125, 155, 147),
-    color(62, 84, 143)
-  );
-
-  //The colors are: [0]sea green, [1]bright yellow, [2]orange red
-  waterColorsTo.push(
-    color(205, 74, 74),
-    color(193, 113, 67),
-    color(255, 214, 101),
-    color(125, 155, 147)
-  );
 
   //Build four arrays: skyColorLerp A/B/C/D to contain the lerpColor() results between the
   //skyColorsFrom[] and skyColorsTo[]
@@ -111,6 +108,8 @@ function setup() {
   h = windowHeight;
   unitX = w / 32; //unit coordinate for x
   unitY = h / 32; //unit coordinate for y
+  unitH = 0;
+  unitH1 = 0;
 
   shadow();
   blurryBg1(); //transition
@@ -118,6 +117,9 @@ function setup() {
 }
 
 function draw() {
+  // Request fresh data from the FFT analysis
+  let spectrum = fft.analyze();
+
   drawSkyEllipse();
 
   waterSurface();
@@ -127,38 +129,52 @@ function draw() {
   strokeWeight(2);
   stroke(43, 49, 45);
 
-  //the building
-  beginShape();
-  vertex(0, 16 * unitY);
-  vertex(0, 13.8 * unitY);
-  vertex(unitX, 13.8 * unitY);
-  vertex(2 * unitX, 11 * unitY);
-  vertex(3 * unitX, 11 * unitY);
-  vertex(3.4 * unitX, 9 * unitY);
-  vertex(4 * unitX, 11 * unitY);
-  vertex(4.7 * unitX, 10.5 * unitY);
-  vertex(4.7 * unitX, 4 * unitY);
-  vertex(4.9 * unitX, 4 * unitY);
-  vertex(5.15 * unitX, 0.5 * unitY);
-  vertex(5.35 * unitX, 0.5 * unitY);
-  vertex(5.75 * unitX, 3 * unitY);
-  vertex(6 * unitX, 4 * unitY);
-  vertex(6 * unitX, 11 * unitY);
-  vertex(6.25 * unitX, 9 * unitY);
-  vertex(7 * unitX, 8 * unitY);
-  vertex(7.5 * unitX, 7 * unitY);
-  vertex(8 * unitX, 8 * unitY);
-  vertex(8.7 * unitX, 9 * unitY);
-  vertex(8.7 * unitX, 10 * unitY);
-  vertex(10 * unitX, 10 * unitY);
-  vertex(10.5 * unitX, 11 * unitY);
-  vertex(11.2 * unitX, 10 * unitY);
-  vertex(11.5 * unitX, 11 * unitY);
-  vertex(12 * unitX, 12 * unitY);
-  vertex(13 * unitX, 13.8 * unitY);
-  vertex(15 * unitX, 13.8 * unitY);
-  vertex(16 * unitX, 16 * unitY);
-  endShape(CLOSE);
+  // Draw the spectrum to show energy
+  for (let i = 0; i < spectrum.length; i++) {
+    unitH1 = map(spectrum[i], 0, 255, 0, -100);
+    unitH = unitY + unitH1 / 8;
+    let r = map(spectrum[i], 0, 255, 0, 71);
+    let c1 = color(r, 41, 50);
+    let c2 = color(43, r, r);
+    let col = lerpColor(c1, c2, random(10));
+
+    fill(col);
+
+    //the building
+    beginShape();
+    vertex(0, 16 * unitY);
+
+    vertex(0, 13.8 * unitH);
+    vertex(unitX, 13.8 * unitH);
+    vertex(2 * unitX, 11 * unitH);
+    vertex(3 * unitX, 11 * unitH);
+    vertex(3.4 * unitX, 9 * unitH);
+    vertex(4 * unitX, 11 * unitH);
+    vertex(4.7 * unitX, 10.5 * unitH);
+    vertex(4.7 * unitX, 4 * unitH);
+    vertex(4.9 * unitX, 4 * unitH);
+    vertex(5.15 * unitX, 0.5 * unitH);
+    vertex(5.35 * unitX, 0.5 * unitH);
+    vertex(5.75 * unitX, 3 * unitH);
+    vertex(6 * unitX, 4 * unitH);
+    vertex(6 * unitX, 11 * unitH);
+    vertex(6.25 * unitX, 9 * unitH);
+    vertex(7 * unitX, 8 * unitH);
+    vertex(7.5 * unitX, 7 * unitH);
+    vertex(8 * unitX, 8 * unitH);
+    vertex(8.7 * unitX, 9 * unitH);
+    vertex(8.7 * unitX, 10 * unitH);
+    vertex(10 * unitX, 10 * unitH);
+    vertex(10.5 * unitX, 11 * unitH);
+    vertex(11.2 * unitX, 10 * unitH);
+    vertex(11.5 * unitX, 11 * unitH);
+    vertex(12 * unitX, 12 * unitH);
+    vertex(13 * unitX, 13.8 * unitH);
+    vertex(15 * unitX, 13.8 * unitH);
+
+    vertex(16 * unitX, 16 * unitY);
+    endShape(CLOSE);
+  }
 
   waterColor(polyShadow, 71, 41, 50, 20);
   waterColor(polyBlurry1, 20, 70, 10, 10); //transition
@@ -183,7 +199,6 @@ function waterSurface() {
       rect(0, 0, 23, 4);
       pop();
     }
-    console.log(y);
 
     if (y < 14) {
       fill(waterColorsLerpA[y % 8]);
@@ -236,7 +251,6 @@ function blurryBg2() {
   v.push(createVector(24 * unitX, 16 * unitY));
   for (let i = 0; i < random(10); i++) {
     let xScale = constrain((random(24, 32) * i) / 2, 24, 32);
-    //let xScale=random(24,32)*i/2;
     let yScale = random(5, 16);
     v.push(createVector(xScale * unitX, yScale * unitY));
   }
@@ -304,9 +318,7 @@ class Poly {
 function waterColor(poly, r, g, b, numLayers) {
   fill(r, g, b, 255 / (2 * numLayers));
   noStroke();
-
   poly = poly.grow().grow();
-
   for (let i = 0; i < numLayers; i++) {
     if (i == int(numLayers / 3) || i == int((2 * numLayers) / 3)) {
       poly = poly.grow().grow();
